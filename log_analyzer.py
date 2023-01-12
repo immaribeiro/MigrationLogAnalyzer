b1db2e2d03d06cb0b0fa82abea9a443f75773141
imma-cx <emanuel.ribeiro@checkmarx.com>
2023-01-12 18:22:20 +0000

    Fix the analyzer

--- log_analyzer.py
+++ log_analyzer.py
@@ -9,23 +9,24 @@ log_file_name = "8f860c2c-3b3a-471f-9501-29c23e792bce.log"
 output_file_path = "outputs/"
 output_file_name = os.path.splitext(log_file_name)[0]
 
+output_file = output_file_path + output_file_name
+
 def from_log_into_csv(log_file_name):
     # Open the log file and the csv file
-    with open(log_file_path + log_file_name, 'r') as logfile, open(output_file_path + output_file_name + "_triages.csv", 'w', newline='') as csvfile_triage, open(output_file_path + output_file_name + "_queries.csv", 'w', newline='') as csvfile_queries:
-        # Create a csv writer object for triages
+    with open(log_file_path + log_file_name, 'r') as logfile, open(output_file + "_triages.csv", 'w', newline='') as csvfile_triage, open(output_file + "_queries.csv", 'w', newline='') as csvfile_queries:
+        # Create a csv writers object for triages
         writer_triage = csv.writer(csvfile_triage)
         # Write the header row
         writer_triage.writerow(['Worker','SimilarityID', 'Error'])
-
+        # Create a csv writers object for queries
         writer_queries = csv.writer(csvfile_queries)
-        writer_triage.writerow(['Worker','Project ID', 'Error'])
+        # Write the header row
+        writer_queries.writerow(['Worker','ProjectID', 'Error'])
         
         for line in logfile:
             try:
-                # Parse each line as a dictionary
                 data = json.loads(line)
                 if "skipping predicate" in data['msg']:
-                    # Extract the similarity id and worker name
                     worker = data['worker']
                     similarity_id = data['msg'].split()[2]
                     msg = data['msg']
@@ -34,15 +35,20 @@ def from_log_into_csv(log_file_name):
                         error = match.group(1)
                     else:
                         error = "No match found."
-                    # Write the extracted data to the csv file
+
                     writer_triage.writerow([worker, similarity_id, error])
                     
                 elif "skipping group" in data['msg']:
-                    project_id = data['msg'].split()[4]
+                    project_id = data['msg'].split()[3]
                     worker = data['worker']
-                    similarity_id = None
-                    # Write the extracted data to the csv file
-                    writer_triage.writerow([similarity_id, worker, project_id])
+                    msg = data['msg']
+                    match = re.search(r'ID \d+ (.+?),', msg)
+                    if match:
+                        error = match.group(1)
+                    else:
+                        error = "No match found."
+                    writer_queries.writerow([worker, project_id, error ])
+
             except json.decoder.JSONDecodeError:
                 # Handle JSONDecodeError in case a line is not in json format
                 print("Line is not in JSON format, skipping")
